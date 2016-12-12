989392ad0e97e32ce48903c961a0cd1548aeb6d0
Ryoma <e165743@ie.u-ryukyu.ac.jp>
2016-12-12 13:48:44 +0900

    1st

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Created by e165743H on 2016/12/12.
+ */
+public class Main {
+    public static void main(String [] args){
+        String str = null;
+        System.out.println(str.length());
+    }
+}
+
