67fa1b46b44b05bba7039057650f071ac91360f6
Ryoma <e165743@ie.u-ryukyu.ac.jp>
2016-12-12 14:15:35 +0900

    3rd

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,8 @@
  */
 public class Main {
     public static void main(String [] args){
-        String str = null;
+        String str = "3．14";
+        Double.parseDouble(str);
         try {
             System.out.println(str.length());
         }catch(NullPointerException e) {
